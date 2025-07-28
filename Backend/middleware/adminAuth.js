0afddea8c2888853f8d797845a24294aa78b6e5c
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-07-28 22:41:47 +0530

    Update adminAuth.js

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 
 const adminAuth = async (req, res, next) => {
     try {
-        let  token  = req.cookies.token;
+        let  {token}  = req.cookies
 
         if (!token) {
             return res.status(400).json({ message: "user does not token for Admin_pannel" })
