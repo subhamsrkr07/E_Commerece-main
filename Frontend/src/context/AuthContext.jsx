8b38f9356692e2a42af2be2c6313f8b3dbfef63a
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-07-28 14:33:18 +0530

    Update AuthContext.jsx

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from 'react'
 export const authDataContext = createContext()
 const AuthContext = ({children}) => {
 
-let serverUrl="http://localhost:8000"
+let serverUrl="https://e-commerece-main-backend-gegv.onrender.com"
 
 let value = {
     serverUrl
