741fed2b2f1efdf3c1d1eb95f8b5b9a7b1c49847
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-07-28 14:50:52 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
     {
-        origin:["https://e-commerece-main-frontend.onrender.com","http://localhost:5174"],
+        origin:["https://e-commerece-main-frontend.onrender.com","https://e-commerece-main-admin.onrender.com"],
         credentials:true
     }
 ))
