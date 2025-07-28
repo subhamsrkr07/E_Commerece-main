4a7bd0a253507d68ae71f2e3de25e74597390427
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-07-28 22:07:38 +0530

    Update authController.js

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -114,8 +114,6 @@ export const adminLogin = async (req, res) => {
                 maxAge: 7 * 24 * 60 * 60 * 1000
             })
             return res.status(200).json(token)
-   
-            
         }
             return res.status(400).json({message:"Invalid Cradentials"})
     } catch (error) {
