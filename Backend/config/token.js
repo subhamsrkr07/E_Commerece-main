9e7586a43e2e02e0e624a4e2160e5a3f142d9e2e
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-07-29 15:14:12 +0530

    Update token.js

--- Backend/config/token.js
+++ Backend/config/token.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
  const genToken = async(userId)=>{ //this is the token part which was not cleared
     try {
-        let token = await jwt.sign({id: user._id}, process.env.JWT_SECRET,{expiresIn:"7d"})
+        let token = await jwt.sign({userId}, process.env.JWT_SECRET,{expiresIn:"7d"})
         return token
     } catch (error) {
         console.log("token error")
