171c91ab692e557f6fad0726e0144eef227cfe49
subhamsrkr07 <121615827+subhamsrkr07@users.noreply.github.com>
2025-08-02 18:14:10 +0530

    Update adminAuth.js

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 
 const adminAuth = async (req, res, next) => {
     try {
-        console.log(req.cookies)
+         return res.status(200).json(req.cookies)
         let  {token}  = req.cookies
         
 
